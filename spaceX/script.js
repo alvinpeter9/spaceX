00e283813631722732cb121fad7ce737e9a91927
Simeon Alvin Ibodje <36511214+alvinpeter9@users.noreply.github.com>
2022-07-12 22:38:21 +0100

    uploaded content
    html, css, js and image file

---
+++ spaceX/script.js
@@ -0,0 +1,51 @@
+const url = 'https://api.spacexdata.com/v4/launches/upcoming';
+const card = document.getElementById('cards');
+
+let strMonth = ['January', 'February', 'March', 'April', 'May', 'June', 
+                'July', 'August', 'September', 'October', 'November', 'December'];
+let currentDate = new Date();
+let cDay = currentDate.getDate();
+let cMonth = currentDate.getMonth() + 1;
+let cYear = currentDate.getFullYear();
+
+
+fetch(url)
+.then( response => {
+  if (!response.ok) {
+    throw new Error(`HTTP error: ${response.status}`);
+  }
+  return response.json();
+})
+.then(data=>data.map(d => {
+  if (d.date_local.slice(5, 7) == cMonth && (d.date_local.slice(8, 10) - cDay) < 4 ) {
+    card.innerHTML += `
+    <div id=${d.id} class=\'card\'>
+     <h2> ${d.name} </h2>
+     <hr>
+     <h3>Set to Launch &#10003;</h3>
+       <div class=\'card-text\'>
+         Launching on ${d.date_local.slice(8, 10) +' '+ strMonth[d.date_local.slice(5, 7)-1] +' '+ d.date_local.slice(0, 4)} 
+       </div>
+       <div class=\'card-text\'>
+         Flight Number: ${d.flight_number}
+       </div>
+     </div>`
+     ;
+  } else {
+    card.innerHTML += `
+    <div id=${d.id} class=\'card\'>
+     <h2> ${d.name} </h2>
+     <hr>
+       <div class=\'card-text\'>
+         Launch Date: ${d.date_local.slice(0, 10)} 
+       </div>
+       <div class=\'card-text\'>
+         Flight Number: ${d.flight_number}
+       </div>
+     </div>`
+    
+  }
+  }
+))
+.catch( error => 
+  card.innerHTML += `<div class='\error\'>unable to retrieve data: ${error}</div>`);
